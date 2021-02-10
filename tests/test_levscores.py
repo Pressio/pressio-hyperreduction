6ff7a498e080df3cf01b75b791046a336a3b8654
Patrick Blonigan <pblonig@sandia.gov>
2021-02-10 06:46:52 -0800

    #4 updated leverage score test to have dofsPerMeshNode=2

--- tests/test_levscores.py
+++ tests/test_levscores.py
@@ -22,14 +22,14 @@ def run():
 
   # random distributed psi by selecting
   # rows of a random matrix
-  psi0 = np.asfortranarray(np.random.rand(15,4))
+  psi0 = np.asfortranarray(np.random.rand(24,4))
   if rank==0:
     print(psi0)
     print("---------\n")
 
-  myNumRows = 5
+  myNumRows = 8
   myStartRow = rank*myNumRows
-  psi = pt.MultiVector(psi0[myStartRow:myStartRow+5, :])
+  psi = pt.MultiVector(psi0[myStartRow:myStartRow+myNumRows, :])
 
   l_scores = pt.Vector(myNumRows)
   leverageScores(l_scores, psi.data())
@@ -41,19 +41,28 @@ def run():
                                    0.44080349621974,
                                    2.35742451093826,
                                    1.15014097270538,
-                                   0.07248648067393])
-  elif rank == 1:
-    l_scores_truth_lcl = np.array([1.43282248176162,
+                                   0.07248648067393,
+                                   1.43282248176162,
                                    2.74311764577994,
-                                   0.41537324804313,
-                                   1.43317716202619,
-                                   1.15354421701282])
-  elif rank == 2:
-    l_scores_truth_lcl = np.array([1.53902367978074,
+                                   0.41537324804313])
+  elif rank == 1:
+    l_scores_truth_lcl = np.array([1.43317716202619, 
+                                   1.15354421701282,
+                                   1.53902367978074,
                                    1.03977559500014,
                                    0.81731094949638,
                                    0.63384371442751,
-                                   1.34991091068034])
+                                   1.34991091068034,
+                                   1.40177954963967])
+  elif rank == 2:
+    l_scores_truth_lcl = np.array([0.91861337644567,
+                                   1.25984161397013,
+                                   2.49581934130582,
+                                   1.38875957598056,
+                                   1.00576547370327,
+                                   1.2727751032992,
+                                   0.73573001951863,
+                                   1.30823090215113])
   else:
     l_scores_truth_lcl = None
 
@@ -62,30 +71,27 @@ def run():
 
   r = l_scores.sumGlobal()
   print(r)
-  assert( r - 17.076572320951822 < 1e-12)
+  assert( r - 28.863887276965883 < 1e-12)
 
-  l_scores_pmf,_ = computeBlockPMF(l_scores,1)
+  l_scores_pmf,_ = computeBlockPMF(l_scores,2)
   print(rank,l_scores_pmf.data())
 
   # check PMF against truth values
   if rank == 0:
-    l_scores_pmf_truth_lcl = np.array([0.0479093632019,
-                                       0.04624000710647,
-                                       0.10235844172836,
-                                       0.06700932378062,
-                                       0.03545572883846])
+    l_scores_pmf_truth_lcl = np.array([0.05792609741518,
+                                       0.10242711535444,
+                                       0.06774265823342,
+                                       0.09638020652212])
   elif rank == 1:
-    l_scores_pmf_truth_lcl = np.array([0.07528620463654,
-                                       0.11365149069605,
-                                       0.04549541247417,
-                                       0.07529658963237,
-                                       0.06710897036787])
+    l_scores_pmf_truth_lcl = np.array([0.08647562386321,
+                                       0.08633839175407,
+                                       0.06680456042989,
+                                       0.08933333113767])
   elif rank == 2:
-    l_scores_pmf_truth_lcl = np.array([0.07839576304273,
-                                       0.06377783869008,
-                                       0.05726410041396,
-                                       0.05189220166225,
-                                       0.0728585637282])
+    l_scores_pmf_truth_lcl = np.array([0.07940335420137,
+                                       0.10895799995126,
+                                       0.08113710519663,
+                                       0.07707355594074])
   else:
     l_scores_pmf_truth_lcl = None
 
@@ -93,16 +99,16 @@ def run():
 
 
   # compute samples
-  mySampleMeshNodes = samplePMF(comm, l_scores_pmf, 10)
+  mySampleMeshNodes = samplePMF(comm, l_scores_pmf, 5)
   print(rank,mySampleMeshNodes)
 
   # check samples against truth values
   if rank == 0:
-    mySampleMeshNodes_truth = [2, 3, 3, 4, 2]
+    mySampleMeshNodes_truth = [2]
   elif rank == 1:
-    mySampleMeshNodes_truth = [2, 0]
+    mySampleMeshNodes_truth = [0, 1]
   elif rank == 2:
-    mySampleMeshNodes_truth = [2, 0, 2]
+    mySampleMeshNodes_truth = [1, 1]
   else:
     mySampleMeshNodes_truth = None
 
